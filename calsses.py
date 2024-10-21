f5ad45de023b6ee0cf30c76ce98b7d757c3ccb12
Justine Jerald Rosel <anxespero@gmail.com>
2024-10-21 16:28:25 +0800

    refactor: refactor comment

--- calsses.py
+++ calsses.py
@@ -1,31 +1,31 @@
 pythonclasses = [
-# Product 1
+# Product Object
     {
     "Product Name": ["Laptop", "Desk Chair", "Smartwatch", "Notebook", "Running Shoes"],
     "Price": ["750", "100", "200", "5", "80"]
 },
 
-# Employee 2
+# Employee Object
     {
     "Employee Name": ["John Doe", "Jane Smith", "Mark Johnson", "Lisa Wong", "Paul McDonald"],
     "Job Titles": ["Sales", "Human Resources", "IT", "Marketing", "Finance"]
 },
 
-# Books 3
+# Books Object
     {
     "Book titles": ["The Great Gatsby", "To Kill a Mockingbird", "1984", "The Catcher in the Rye",
                     "A Brief History of Time"],
     "Book Author": ["F. Scott Fitzgerald", "Harper Lee", "George Orwell", "J.D. Salinger", "Stephen Hawking"]
 },
 
-# University 4
+# University Object
     {
     "University Name": ["University of the Philippines", "Ateneo de Manila University", "De La Salle University",
                         "University of Santo Tomas", "Polytechnic University of the Philippines"],
     "University Location": ["Quezon City", "Quezon City", "Manila", "Manila", "Manila"]
 },
 
-# Restaurant 5
+# Restaurant Object
     {
     "Restaurant Name": ["Vikings Luxury Buffet", "Antonio's Restaurant", "Mesa Filipino Moderne", "Manam Comfort Filipino"
                         , "Ramen Nagi"],
