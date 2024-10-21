c11db0a89f86e05bea2baa9e68519f4350653108
Justine Jerald Rosel <anxespero@gmail.com>
2024-10-21 16:33:34 +0800

    refactor: fix typo

--- calsses.py
+++ calsses.py
@@ -2,18 +2,18 @@ pythonclasses = [
 # Product Object
     {
     "Product Name": ["Laptop", "Desk Chair", "Smartwatch", "Notebook", "Running Shoes"],
-    "Price": ["750", "100", "200", "5", "80"]
+    "Product Price": ["750", "100", "200", "5", "80"]
 },
 
 # Employee Object
     {
     "Employee Name": ["John Doe", "Jane Smith", "Mark Johnson", "Lisa Wong", "Paul McDonald"],
-    "Job Titles": ["Sales", "Human Resources", "IT", "Marketing", "Finance"]
+    "Job Title": ["Sales", "Human Resources", "IT", "Marketing", "Finance"]
 },
 
 # Books Object
     {
-    "Book titles": ["The Great Gatsby", "To Kill a Mockingbird", "1984", "The Catcher in the Rye",
+    "Book Title": ["The Great Gatsby", "To Kill a Mockingbird", "1984", "The Catcher in the Rye",
                     "A Brief History of Time"],
     "Book Author": ["F. Scott Fitzgerald", "Harper Lee", "George Orwell", "J.D. Salinger", "Stephen Hawking"]
 },
